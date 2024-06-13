dd8e03a84ce77a54633da488ce58faca1098b088
João Victor <jvna07@gmail.com>
2024-06-12 21:13:22 -0300

    Revert "Hello World"
    This reverts commit 81962267cee0fb1344a1384316e8a4f2e052c8d7.

--- GitHubDesktopExemplo/src/githubdesktopexemplo/GitHubDesktopExemplo.java
+++ GitHubDesktopExemplo/src/githubdesktopexemplo/GitHubDesktopExemplo.java
@@ -1,3 +1,7 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
 package githubdesktopexemplo;
 
 /**
@@ -6,8 +10,11 @@ package githubdesktopexemplo;
  */
 public class GitHubDesktopExemplo {
 
+    /**
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        // TODO code application logic here
     }
     
 }
