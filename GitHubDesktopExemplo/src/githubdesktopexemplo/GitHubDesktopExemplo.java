81962267cee0fb1344a1384316e8a4f2e052c8d7
João Victor <jvna07@gmail.com>
2024-06-12 21:01:40 -0300

    Hello World

--- GitHubDesktopExemplo/src/githubdesktopexemplo/GitHubDesktopExemplo.java
+++ GitHubDesktopExemplo/src/githubdesktopexemplo/GitHubDesktopExemplo.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package githubdesktopexemplo;
 
 /**
@@ -10,11 +6,8 @@ package githubdesktopexemplo;
  */
 public class GitHubDesktopExemplo {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hello World!");
     }
     
 }
